16a1f0ea2f3c2addfb8ce7ece33c3f6e67737cfe
dimitkos <User1@DESKTOP-D6C1HHM>
2020-11-30 11:49:25 +0200

    Add all fluent entities in db

--- Lab.DataAcces/Data/ApplicationDbContext.cs
+++ Lab.DataAcces/Data/ApplicationDbContext.cs
@@ -17,6 +17,9 @@ namespace Lab.DataAccess.Data
         public DbSet<Author> Authors { get; set; }
         public DbSet<Publisher> Publishers { get; set; }
         public DbSet<Fluent_BookDetail> Fluent_BookDetails { get; set; }
+        public DbSet<Fluent_Book> Fluent_Books { get; set; }
+        public DbSet<Fluent_Author> Fluent_Authors { get; set; }
+        public DbSet<Fluent_Publisher> Fluent_Publishers { get; set; }
 
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
@@ -24,6 +27,23 @@ namespace Lab.DataAccess.Data
             //BookDetails
             modelBuilder.Entity<Fluent_BookDetail>().HasKey(x => x.BookDetail_Id);
             modelBuilder.Entity<Fluent_BookDetail>().Property(x => x.NumberOfChapters).IsRequired();
+
+            //Book
+            modelBuilder.Entity<Fluent_Book>().HasKey(x => x.Book_Id);
+            modelBuilder.Entity<Fluent_Book>().Property(x => x.ISBN).IsRequired().HasMaxLength(15);
+            modelBuilder.Entity<Fluent_Book>().Property(x => x.Title).IsRequired();
+            modelBuilder.Entity<Fluent_Book>().Property(x => x.Price).IsRequired();
+
+            //Author
+            modelBuilder.Entity<Fluent_Author>().HasKey(x => x.Author_Id);
+            modelBuilder.Entity<Fluent_Author>().Property(x => x.FirstName).IsRequired();
+            modelBuilder.Entity<Fluent_Author>().Property(x => x.LastName).IsRequired();
+            modelBuilder.Entity<Fluent_Author>().Ignore(x => x.FullName);
+
+            //Publisher
+            modelBuilder.Entity<Fluent_Publisher>().HasKey(x => x.Publisher_Id);
+            modelBuilder.Entity<Fluent_Publisher>().Property(x => x.Name).IsRequired();
+            modelBuilder.Entity<Fluent_Publisher>().Property(x => x.Location).IsRequired();
         }
     }
 }
--- Lab.DataAcces/Migrations/20201130094323_addfluentbooksauthorpublishers.Designer.cs
+++ Lab.DataAcces/Migrations/20201130094323_addfluentbooksauthorpublishers.Designer.cs
@@ -4,14 +4,16 @@ using Lab.DataAccess.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Lab.DataAccess.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20201130094323_addfluentbooksauthorpublishers")]
+    partial class addfluentbooksauthorpublishers
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
