ccc113f89c248b4f14665c00b0cb41eb9e0c448b
dimitkos <User1@DESKTOP-D6C1HHM>
2020-12-04 10:45:11 +0200

    Seed catefgory db, add output logs

---
+++ Lab.DataAcces/Migrations/20201204082618_addrawcategorytable.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Lab.DataAccess.Migrations
+{
+    public partial class addrawcategorytable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("INSERT INTO tbl_category VALUES('Cat 1')");
+            migrationBuilder.Sql("INSERT INTO tbl_category VALUES('Cat 2')");
+            migrationBuilder.Sql("INSERT INTO tbl_category VALUES('Cat 3')");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
