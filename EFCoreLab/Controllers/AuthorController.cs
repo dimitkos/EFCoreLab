989d865f0e93a319ab37f56d5cf02d29d2381209
dimitkos <User1@DESKTOP-D6C1HHM>
2020-12-07 17:23:40 +0200

    Implement authro and publisher functionality

---
+++ EFCoreLab/Controllers/AuthorController.cs
@@ -0,0 +1,75 @@
+﻿using Lab.DataAccess.Data;
+using Lab.Model.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace EFCoreLab.Controllers
+{
+    public class AuthorController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public AuthorController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            List<Author> authors = _context.Authors.ToList();
+            return View(authors);
+        }
+
+        public IActionResult Upsert(int? id)
+        {
+
+            Author author = new Author();
+
+            if (id == null)
+            {
+                //for create, we dont need any id
+                return View(author);
+            }
+
+            author = _context.Authors.FirstOrDefault(x => x.Author_Id == id);
+
+            if (author == null)
+                return NotFound();
+
+            return View(author);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Upsert(Author author)
+        {
+            if (ModelState.IsValid)
+            {
+                if (author.Author_Id == 0)
+                {
+                    //this is create
+                    _context.Authors.Add(author);
+                }
+                else
+                {
+                    //this is update
+                    _context.Authors.Update(author);
+                }
+                _context.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(author);
+        }
+
+        public IActionResult Delete(int id)
+        {
+            var category = _context.Authors.FirstOrDefault(x => x.Author_Id == id);
+            _context.Authors.Remove(category);
+            _context.SaveChanges();
+
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
---
+++ EFCoreLab/Controllers/PublisherController.cs
@@ -0,0 +1,75 @@
+﻿using Lab.DataAccess.Data;
+using Lab.Model.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace EFCoreLab.Controllers
+{
+    public class PublisherController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public PublisherController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            List<Publisher> publishers = _context.Publishers.ToList();
+            return View(publishers);
+        }
+
+        public IActionResult Upsert(int? id)
+        {
+
+            Publisher publisher = new Publisher();
+
+            if (id == null)
+            {
+                //for create, we dont need any id
+                return View(publisher);
+            }
+
+            publisher = _context.Publishers.FirstOrDefault(x => x.Publisher_Id == id);
+
+            if (publisher == null)
+                return NotFound();
+
+            return View(publisher);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Upsert(Publisher publisher)
+        {
+            if (ModelState.IsValid)
+            {
+                if (publisher.Publisher_Id == 0)
+                {
+                    //this is create
+                    _context.Publishers.Add(publisher);
+                }
+                else
+                {
+                    //this is update
+                    _context.Publishers.Update(publisher);
+                }
+                _context.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(publisher);
+        }
+
+        public IActionResult Delete(int id)
+        {
+            var category = _context.Publishers.FirstOrDefault(x => x.Publisher_Id == id);
+            _context.Publishers.Remove(category);
+            _context.SaveChanges();
+
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
