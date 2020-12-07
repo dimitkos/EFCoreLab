2c1cf6c0cbc87b34da53a4406d029c24533d683a
dimitkos <User1@DESKTOP-D6C1HHM>
2020-12-07 22:45:33 +0200

    Add book index

---
+++ EFCoreLab/Controllers/BookController.cs
@@ -0,0 +1,75 @@
+﻿using Lab.DataAccess.Data;
+using Lab.Model.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace EFCoreLab.Controllers
+{
+    public class BookController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public BookController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            List<Book> books = _context.Books.ToList();
+            return View(books);
+        }
+
+        //public IActionResult Upsert(int? id)
+        //{
+
+        //    Author author = new Author();
+
+        //    if (id == null)
+        //    {
+        //        //for create, we dont need any id
+        //        return View(author);
+        //    }
+
+        //    author = _context.Authors.FirstOrDefault(x => x.Author_Id == id);
+
+        //    if (author == null)
+        //        return NotFound();
+
+        //    return View(author);
+        //}
+
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public IActionResult Upsert(Author author)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        if (author.Author_Id == 0)
+        //        {
+        //            //this is create
+        //            _context.Authors.Add(author);
+        //        }
+        //        else
+        //        {
+        //            //this is update
+        //            _context.Authors.Update(author);
+        //        }
+        //        _context.SaveChanges();
+        //        return RedirectToAction(nameof(Index));
+        //    }
+
+        //    return View(author);
+        //}
+
+        //public IActionResult Delete(int id)
+        //{
+        //    var category = _context.Authors.FirstOrDefault(x => x.Author_Id == id);
+        //    _context.Authors.Remove(category);
+        //    _context.SaveChanges();
+
+        //    return RedirectToAction(nameof(Index));
+        //}
+    }
+}
